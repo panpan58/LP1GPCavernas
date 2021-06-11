af4eb0f7020ac5a8a6b3ec174a9e247140e361f3
António Branco <a21906811@alunos.ulht.pt>
2021-06-11 09:44:59 +0100

    Create proj, Implement console input

---
+++ GP-Cavernas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GP_Cavernas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console Variables
+            int lin, cols, n;
+            lin = Convert.ToInt32(args[0]);
+            cols = Convert.ToInt32(args[1]);
+            n = Convert.ToInt32(args[2]);
+
+            //Arrays
+            int[,] world, newWolrd, auxWolrd;
+        }
+    }
+}
